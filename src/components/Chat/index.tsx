758afafeadedf56e482eb089bd75701be0cdf2da
ReemDalal <reem.dalal.98@gmail.com>
2025-09-28 22:39:49 +0300

    init

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,80 @@
+import './index.css'
+
+const Chat = () => {
+  return (
+    <div className="page-container">
+      <div className="chat-container">
+        <div className="chat">
+          <div className="user-section">
+            <div className="avatar" />
+            <div className="user-text-bubble">
+              Hello World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!
+            </div>
+          </div>
+          <div className="bot-section">
+            <div className="avatar" />
+            <div className="bot-text-bubble">
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello!Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello!
+            </div>
+          </div>
+          <div className="user-section">
+            <div className="avatar" />
+            <div className="user-text-bubble">
+              Hello World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!
+            </div>
+          </div>
+          <div className="bot-section">
+            <div className="avatar" />
+            <div className="bot-text-bubble">
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello!Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello!
+            </div>
+          </div>
+          <div className="user-section">
+            <div className="avatar" />
+            <div className="user-text-bubble">
+              Hello World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!Hello World!Hello World!Hello World!Hello
+              World!Hello World!
+            </div>
+          </div>
+          <div className="bot-section">
+            <div className="avatar" />
+            <div className="bot-text-bubble">
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello!Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello! Hello!
+              Hello! Hello! Hello!
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="general-info-container">
+        <p className="general-info-text">
+          This is a bot to help you talk about your feelings, general guidelines
+          blah blah blah{' '}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
